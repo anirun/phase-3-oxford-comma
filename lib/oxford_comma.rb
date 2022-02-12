979029ae5260ca3d2dc2b3fe2bb9f22b5340448d
Nurina Junus <nurina.junus@gmail.com>
2022-02-12 16:02:23 -0700

    commit tree 7271069bb48a3203d74cef6f2ab30c6c4fc868ee

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    x = array.length
+    if x > 2 
+        array[-1] = "and " + array[-1]
+        return array.join(", ")
+    elsif x == 2
+        array[-1] = "and " + array[-1]
+        return array.join(" ")
+    elsif x == 1
+        return array.join
+        binding.pry
+    end
+    
 end
